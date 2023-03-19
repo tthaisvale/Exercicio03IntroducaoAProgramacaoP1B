3cd7ef8be0d257c01f16f7a32ee0b9de57cf14ca
Thais Vale <126836029+tthaisvale@users.noreply.github.com>
2023-03-19 14:01:47 -0300

    Add files via upload

---
+++ Questao_03.py
@@ -0,0 +1,4 @@
+n1 = int(input('Digite um valor'))
+n2 = 1357
+s = (((n1+n2)*8)/5)**2
+print('O resultado final foi de',(s))
---
+++ Questao_04.py
@@ -0,0 +1,3 @@
+nome = input('Olá, por favor, digite seu nome ')
+nome = nome.upper()
+print(f'Olá {nome}, você está covidado para o meu aniversário no dia 28 de janeiro às 21h!')
